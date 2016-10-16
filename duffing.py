e5a1a8a58768a1b53b96ee6b50817d0611c7dcdb
jagsanchez <jagasanchez@gmail.com>
2016-10-16 21:47:36 +0200

    Primera versión del documento.

--- duffing.py
+++ duffing.py
@@ -1,4 +1,3 @@
-
 from scipy.integrate import odeint
 from pylab import *
 
@@ -10,8 +9,8 @@ gamma = .5
 omega = pi/2.
 
 def dX_dt(X, t):
-	return array([X[1], -delta*X[1] -alfa*X[0] - beta*X[0]**3 + gamma * cos(omega * t)])
-	
+    return array([X[1], -delta*X[1] -alfa*X[0] - beta*X[0]**3 + gamma * cos(omega * t)])
+
 t = linspace(0, 100, 1001)
 X = odeint(dX_dt, [1., 0.], t)
 x, v = X.T
